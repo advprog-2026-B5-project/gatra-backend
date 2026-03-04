f2242fd3360f09ff02a166e0709f692096630b22
Inaeah <nayahsaffanah@gmail.com>
2026-03-04 15:55:13 +0700

    fix add Article and Category model

--- src/main/java/id/ac/ui/cs/advprog/gatra/model/Category.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/model/Category.java
@@ -1,4 +1,31 @@
 package id.ac.ui.cs.advprog.gatra.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "category")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
 }
