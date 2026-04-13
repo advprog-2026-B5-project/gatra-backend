5765eeb728ea4b5c55cd138d0413c6cacb2c7884
kanayradeeva010 <adeevakanayra@gmail.com>
2026-04-13 22:23:27 +0700

    test(quiz): add createQuestion test

--- src/test/java/id/ac/ui/cs/advprog/gatra/quiz/service/QuizServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/gatra/quiz/service/QuizServiceTest.java
@@ -1,4 +1,46 @@
 package id.ac.ui.cs.advprog.gatra.quiz.service;
 
-public class QuizServiceTest {
+import id.ac.ui.cs.advprog.gatra.quiz.dto.CreateQuestionRequest;
+import id.ac.ui.cs.advprog.gatra.quiz.model.Question;
+import id.ac.ui.cs.advprog.gatra.quiz.repository.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class QuizServiceTest {
+
+    @Mock
+    private QuestionRepository questionRepository;
+
+    @InjectMocks
+    private QuizServiceImpl quizService;
+
+    @Test
+    void shouldCreateQuestionSuccessfully() {
+
+        CreateQuestionRequest request = new CreateQuestionRequest(
+                "Ibukota Indonesia?",
+                List.of("Jakarta", "Bandung"),
+                "Jakarta",
+                UUID.randomUUID()
+        );
+
+        Question savedQuestion = mock(Question.class);
+
+        when(questionRepository.save(any())).thenReturn(savedQuestion);
+
+        Question result = quizService.createQuestion(request);
+
+        assertNotNull(result);
+        verify(questionRepository, times(1)).save(any());
+    }
 }
