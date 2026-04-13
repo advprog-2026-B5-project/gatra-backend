41bab9e2f8620e40e9f88ab60103bc8f999df7b2
kanayradeeva010 <adeevakanayra@gmail.com>
2026-04-13 21:57:34 +0700

    feat(quiz): add quizservice skeleton

--- src/main/java/id/ac/ui/cs/advprog/gatra/quiz/service/QuizService.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/quiz/service/QuizService.java
@@ -1,4 +1,18 @@
 package id.ac.ui.cs.advprog.gatra.quiz.service;
 
-public class QuizService {
+import id.ac.ui.cs.advprog.gatra.quiz.dto.CreateQuestionRequest;
+import id.ac.ui.cs.advprog.gatra.quiz.model.Question;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface QuizService {
+
+    Question createQuestion(CreateQuestionRequest request);
+
+    List<Question> getAllQuestions();
+
+    Question getQuestionById(UUID id);
+
+    void deleteQuestion(UUID id);
 }
--- src/main/java/id/ac/ui/cs/advprog/gatra/quiz/service/QuizServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/quiz/service/QuizServiceImpl.java
@@ -1,4 +1,38 @@
 package id.ac.ui.cs.advprog.gatra.quiz.service;
 
-public class QuizServiceImpl {
+import id.ac.ui.cs.advprog.gatra.quiz.dto.CreateQuestionRequest;
+import id.ac.ui.cs.advprog.gatra.quiz.model.Question;
+import id.ac.ui.cs.advprog.gatra.quiz.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class QuizServiceImpl implements QuizService {
+
+    private final QuestionRepository questionRepository;
+
+    @Override
+    public Question createQuestion(CreateQuestionRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+    @Override
+    public Question getQuestionById(UUID id) {
+        return questionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Question not found"));
+    }
+
+    @Override
+    public void deleteQuestion(UUID id) {
+        questionRepository.deleteById(id);
+    }
 }
