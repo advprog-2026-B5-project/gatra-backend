5c1e9250f826251ffa478d1746e54971cb4cf2f3
roseliaevanny <roseliasucipto8@gmail.com>
2026-04-15 21:47:25 +0700

    add MissionProgressService and implementation

---
+++ src/main/java/id/ac/ui/cs/advprog/gatra/service/MissionProgressService.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.gatra.service;
+
+import id.ac.ui.cs.advprog.gatra.dto.MissionProgressResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MissionProgressService {
+    List<MissionProgressResponse> getActiveMissionsWithProgress(UUID userId);
+    void incrementProgress(UUID userId, String actionType);
+    MissionProgressResponse claimReward(UUID userId, UUID missionId);
+}
