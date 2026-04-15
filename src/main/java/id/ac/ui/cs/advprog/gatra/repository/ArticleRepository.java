50f012ad1bef96c104007f54007f31120900f9f1
kanayradeeva010 <adeevakanayra@gmail.com>
2026-04-15 11:13:15 +0700

    refactor: simplify ArticleRepository without native queries

--- src/main/java/id/ac/ui/cs/advprog/gatra/repository/ArticleRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/repository/ArticleRepository.java
@@ -2,17 +2,8 @@ package id.ac.ui.cs.advprog.gatra.repository;
 
 import id.ac.ui.cs.advprog.gatra.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ArticleRepository extends JpaRepository<Article, UUID> {
-    @Query(value = "SELECT * FROM articles WHERE id = :id", nativeQuery = true)
-    Optional<Article> findByIdIncludeDeleted(@Param("id") UUID id);
 
-    @Query(value = "SELECT * FROM articles WHERE deleted_at IS NOT NULL ORDER BY deleted_at DESC", nativeQuery = true)
-    List<Article> findAllDeleted();
 }
