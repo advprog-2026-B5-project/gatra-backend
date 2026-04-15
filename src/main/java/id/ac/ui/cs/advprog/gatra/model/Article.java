9d1bb399a23b18b65c339c46102dca8ecb8e2d96
kanayradeeva010 <adeevakanayra@gmail.com>
2026-04-15 11:11:51 +0700

    refactor: remove SQLRestriction from Article entity

--- src/main/java/id/ac/ui/cs/advprog/gatra/model/Article.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/model/Article.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "articles")
-@SQLRestriction("deleted_at IS NULL")
 
 @Data
 @Builder
