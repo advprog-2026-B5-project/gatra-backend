dd7bbae8aefebfc77496f76b87dee36aa1e8b6ca
kanayradeeva010 <adeevakanayra@gmail.com>
2026-04-15 20:10:10 +0700

    refactor: add toResponseFromUserAchievement to AchievementMapper

--- src/main/java/id/ac/ui/cs/advprog/gatra/mapper/AchievementMapper.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/mapper/AchievementMapper.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.gatra.mapper;
 
 import id.ac.ui.cs.advprog.gatra.dto.AchievementResponse;
 import id.ac.ui.cs.advprog.gatra.model.Achievement;
+import id.ac.ui.cs.advprog.gatra.model.UserAchievement;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,5 +16,12 @@ public class AchievementMapper {
                 .description(achievement.getDescription())
                 .badgeUrl(achievement.getBadgeUrl())
                 .build();
+    }
+
+    public AchievementResponse toResponseFromUserAchievement(UserAchievement userAchievement) {
+        AchievementResponse response = toResponse(userAchievement.getAchievement());
+        response.setUnlockedAt(userAchievement.getUnlockedAt().toString());
+        response.setDisplayed(userAchievement.isDisplayed());
+        return response;
     }
 }
