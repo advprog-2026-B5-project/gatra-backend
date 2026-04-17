2d906cb556458bd3731e78753c301c1adbedc20a
Rehema Zurafa Saputra <rehemazurafasaputra@gmail.com>
2026-04-17 14:29:08 +0700

    fix: memperbaiki unit testing nggak jalan sama infinite loop saat pengambilan data.

--- src/main/java/id/ac/ui/cs/advprog/gatra/model/UserAchievement.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/model/UserAchievement.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.gatra.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
@@ -18,10 +19,12 @@ public class UserAchievement {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "achievement_id", nullable = false)
+    @JsonIgnore
     private Achievement achievement;
 
     @Column(name = "unlocked_at", nullable = false)
