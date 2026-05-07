7b58d80ead9f280baedcc360b22968c1a76b8328
Kanayra Maritza Sanika Adeeva <kanayra.maritza@ui.ac.id>
2026-05-07 11:48:51 +0700

    feat: implement leaderboard service grouped by tier with buff debuff applied

--- src/main/java/id/ac/ui/cs/advprog/gatra/clan/service/LeaderboardService.java
+++ src/main/java/id/ac/ui/cs/advprog/gatra/clan/service/LeaderboardService.java
@@ -2,6 +2,9 @@ package id.ac.ui.cs.advprog.gatra.clan.service;
 
 import id.ac.ui.cs.advprog.gatra.clan.dto.TierLeaderboardResponse;
 
+import java.util.List;
+
 public interface LeaderboardService {
     TierLeaderboardResponse getLeaderboardByTier(String tier);
+    List<TierLeaderboardResponse> getAllTierLeaderboards();
 }
